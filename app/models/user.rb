9f3de2b403604125820b5782081981bb8fea6307
imotasatara <satommy6@gmail.com>
2023-04-17 22:23:10 +0900

    ユーザーモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :last_name, :first_name, :last_name_kana, :first_name_kana,
+              :address, :phone_number, :relationship_id, :emergency_contact
+  end
+
+  has_many :informations
 end
