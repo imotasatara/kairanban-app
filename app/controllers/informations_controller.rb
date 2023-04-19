e161eb29b1a2c38212decbd648a6392d8f75ad92
imotasatara <satommy6@gmail.com>
2023-04-19 22:50:56 +0900

    新規投稿機能の設定

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -3,8 +3,20 @@ class InformationsController < ApplicationController
   end
 
   def new
+    @information = Information.new
   end
 
   def create
+    @information = Information.new(information_params)
+    if @information.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def information_params
+    params.require(:information).permit(:title, :content, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/information.rb
+++ app/models/information.rb
@@ -1,4 +1,10 @@
 class Information < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :title, :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
